024f3e4064ccf906f28ba5f03bb929f00bafe9ec
unknown <davidringborg@gmail.com>
2020-08-25 11:16:56 +0200

    Updates

---
+++ precache-manifest.fd6e68364e86258f18907ed7c53af8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d7063cd4a4ec1c899245d9a2fb6866",
+    "url": "/MedingPortfolio/index.html"
+  },
+  {
+    "revision": "0d9c79c4582f4cb9859c",
+    "url": "/MedingPortfolio/static/css/main.c02389a7.chunk.css"
+  },
+  {
+    "revision": "31a6157d861d48e37a2f",
+    "url": "/MedingPortfolio/static/js/2.ea68924e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MedingPortfolio/static/js/2.ea68924e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9c79c4582f4cb9859c",
+    "url": "/MedingPortfolio/static/js/main.c4d18b8c.chunk.js"
+  },
+  {
+    "revision": "18a493d1b260715c4a42",
+    "url": "/MedingPortfolio/static/js/runtime-main.0e954930.js"
+  },
+  {
+    "revision": "45b141e77217d24e8b20d09ada2e1705",
+    "url": "/MedingPortfolio/static/media/david.45b141e7.png"
+  }
+]);
